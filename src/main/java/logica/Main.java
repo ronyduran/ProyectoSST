ca410250915cdf357dfcd642bb55cd263ba0fd64
ronyd <ronydurannunez@hotmail.com>
2020-07-27 02:44:35 -0400

    Adicion Thymeleaf

--- src/main/java/logica/Main.java
+++ src/main/java/logica/Main.java
@@ -1,5 +1,13 @@
 package logica;
-import  io.javalin.Javalin;
+
+import io.javalin.Javalin;
+import io.javalin.plugin.rendering.JavalinRenderer;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +16,17 @@ public class Main {
             javalinConfig.addStaticFiles("/Web SST");
         } ).start(7000);
 
+        JavalinRenderer.register(JavalinThymeleaf.INSTANCE, ".html");
 
+        app.routes(() -> {
+            path( "/", () -> {
+                get("/",ctx -> {
+                    Map<String,Object> modelo = new HashMap<>();
+                    modelo.put("esta","Encendido");
+                    modelo.put("numero","3");
+                    modelo.put("taman",88);
+                    ctx.render("/Web SST/index.html", modelo);});
+            });
+        });
     }
 }
