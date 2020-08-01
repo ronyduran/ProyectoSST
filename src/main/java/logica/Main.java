9cd7f6d9d79c803b0d2d4f3daebaa161eb75882b
ronyd <ronydurannunez@hotmail.com>
2020-08-01 10:13:34 -0400

    Se agrego el metodo post en el main y en el html, el th:each pera visualizar una lista de string

--- src/main/java/logica/Main.java
+++ src/main/java/logica/Main.java
@@ -3,9 +3,9 @@ package logica;
 import io.javalin.Javalin;
 import io.javalin.plugin.rendering.JavalinRenderer;
 import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
@@ -18,14 +18,32 @@ public class Main {
 
         JavalinRenderer.register(JavalinThymeleaf.INSTANCE, ".html");
 
+        List<String> notis=new ArrayList<>();
+
+        notis.add("Alarma 1");
+        notis.add("Alarma 2");
+        notis.add("Alarma 3");
+
+
         app.routes(() -> {
             path( "/", () -> {
                 get("/",ctx -> {
+
+
                     Map<String,Object> modelo = new HashMap<>();
                     modelo.put("esta","Encendido");
                     modelo.put("numero","3");
-                    modelo.put("taman",88);
+                    modelo.put("taman",75);
+                    modelo.put("notifi",notis);
                     ctx.render("/Web SST/index.html", modelo);});
+
+                post("/",ctx -> {
+                    notis.add(ctx.formParam("notfi",String.class).get());
+
+                    System.out.println(ctx.formParam("notfi",String.class).get());
+                });
+
+
             });
         });
     }
